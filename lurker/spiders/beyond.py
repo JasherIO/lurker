c12ea9c2c4bd26f7989c21f59bf6c9cf5b933e5e
Willis <3475947+JasherIO@users.noreply.github.com>
2018-12-27 10:50:43 -0600

    Check for PS picture

--- lurker/spiders/beyond.py
+++ lurker/spiders/beyond.py
@@ -7,14 +7,17 @@ TEAMS_SELECTOR = '#ipsTabs_elTourneyTabs_standings_panel > div > div > table > t
 TEAM_NAME_SELECTOR = '#ipsLayout_mainArea > section > div:nth-child(1) > div.title-section > h3::text'
 PLAYERS_SELECTOR = '#ipsTabs_elTeamTabs_details_panel > div > div > table > tbody > tr'
 PLAYER_DISPLAY_NAME_SELECTOR = 'td:nth-child(1) > a::text'
-PLAYER_PLATFORM_SELECTOR = 'td:nth-child(3) > div > span > img::attr(src)'
+PLAYER_STEAM_SELECTOR = 'td:nth-child(3) > div > span > img::attr(src)'
+PLAYER_PS_SELECTOR = 'td:nth-child(3) > span > img::attr(src)'
 PLAYER_STEAM_ID_SELECTOR = 'td:nth-child(3) > div > ul > li > ul > li > a::text'
 PLAYER_PS_ID_SELECTOR = 'td:nth-child(3)::text'
+
+
 
 STEAM_PICTURE = 'https://teambeyond.net/forum/uploads/set_resources_2/a6a2e7cb1d0d4e506cc9e64a9611c0f2_Steam20.png'
 PS_PICTURE = 'https://teambeyond.net/forum/uploads/set_resources_2/a6a2e7cb1d0d4e506cc9e64a9611c0f2_PS20.png'
 
-# scrapy crawl beyond -a tournament="https://teambeyond.net/forum/tournaments/164-astronauts-2000-rocket-league-3v3-1217-700pm-est/standings/" -o entrants.csv
+# scrapy crawl beyond -a tournament="https://teambeyond.net/forum/tournaments/164-astronauts-2000-rocket-league-3v3-1217-700pm-est/standings/"
 class BeyondSpider(scrapy.Spider):
     name = 'beyond'
     allowed_domains = ['teambeyond.net']
@@ -37,12 +40,13 @@ class BeyondSpider(scrapy.Spider):
 
         for player in response.css(PLAYERS_SELECTOR):
             displayName = player.css(PLAYER_DISPLAY_NAME_SELECTOR).extract_first(default='').strip()
-            platform = player.css(PLAYER_PLATFORM_SELECTOR).extract_first(default='')
+            platform = player.css(PLAYER_STEAM_SELECTOR).extract_first(default='') or player.css(PLAYER_PS_SELECTOR).extract_first(default='')
             platformId = player.css(PLAYER_STEAM_ID_SELECTOR).extract_first(default='').strip() or player.css(PLAYER_PS_ID_SELECTOR).extract_first(default='').strip()
             
             if platform == STEAM_PICTURE:
                 platform = "steam"
-            elif platform == PS_PICTURE:
+            
+            if platform == PS_PICTURE:
                 platform = "ps"
             
             if validators.url(platformId):
